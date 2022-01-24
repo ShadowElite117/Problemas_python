8f03fb38bb80a59c1e5c6a78c8d41250e104d87f
Luis Ferreira <lefelite@gmail.com>
2022-01-23 23:24:56 -0500

    Cadenas 75.

---
+++ Cadenas/75.py
@@ -0,0 +1,29 @@
+import os
+
+def borrar_pantalla():
+    if os.name == "posix":
+        return os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        return os.system ("cls")
+
+def invertir_cadena(cadena):
+    cadena_invertida = ""
+    lon_cadena = len(cadena)
+
+    for i in range(lon_cadena):
+        cadena_invertida += cadena[-i - 1]
+
+    return cadena_invertida
+
+def main():
+    borrar_pantalla()
+
+    cadena = input("Introduzca la cadena de texto que desea invertir: ")
+    cadena_invertida = invertir_cadena(cadena)
+
+    if cadena == cadena_invertida:
+        print('La cadena "' + cadena + '" es palíndrome.')
+    else:
+        print('La cadena "' + cadena + '" no es palíndrome.')
+
+main()
