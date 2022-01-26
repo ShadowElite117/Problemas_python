fb15b0daa0bfadf9c16443ef27d11ff2ee854fdb
Luis Ferreira <lefelite@gmail.com>
2022-01-26 06:49:07 -0500

    Numéricos 12.

---
+++ Numéricos/12.py
@@ -0,0 +1,78 @@
+import os
+
+class Error(Exception):
+    pass
+
+class CoeficienteCero(Error):
+    pass
+
+def borrar_pantalla():
+    if os.name == "posix":
+        return os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        return os.system ("cls")
+
+def detectar_errores(n):
+    while True:
+        try:
+            if n == 1:
+                var = input("Introduzca el coeficiente A del término x^2: ")
+            elif n == 2:
+                var = input("Introduzca el coeficiente B del término x: ")
+            elif n == 3:
+                var = input("Introduzca el coeficiente C del término independiente: ")
+            elif n == 4:
+                var = input("Introduzca el número que desea evaluar en la derivada del polinomio: ")
+
+            var = int(var)
+
+            if n == 1 and var == 0:
+                raise CoeficienteCero
+
+        except ValueError:
+            try:
+                var = float(var)
+
+                if n == 1 and var == 0:
+                    raise CoeficienteCero
+                else:
+                    break
+            except ValueError:
+                borrar_pantalla()
+                print("Por favor introduzca un número racional.")
+            except CoeficienteCero:
+                print("El coeficiente A del término x^2 no puede ser cero, ya que el polinomio debe ser de grado 2.")
+        except CoeficienteCero:
+            print("El coeficiente A del término x^2 no puede ser cero, ya que el polinomio debe ser de grado 2.")
+        else:
+            break
+
+    return var
+
+# La derivada de un polinomio grado 2 de la forma Ax^2 + Bx + C, es 2Ax + B
+def derivar_polinomio_g2(A):
+    A = A * 2
+
+    return A
+
+def evaluar_derivada(A, B, x):
+    resultado = A * x + B
+
+    return resultado
+
+def main():
+    borrar_pantalla()
+
+    # Polinomio de grado 2, de la forma Ax^2 + Bx + C
+    A = detectar_errores(1)
+    B = detectar_errores(2)
+    C = detectar_errores(3)
+    x = detectar_errores(4)
+
+    A_1 = derivar_polinomio_g2(A)
+    resultado = evaluar_derivada(A_1, B, x)
+
+    print("La derivada del polinomio (" + str(A) + ")x^2 + (" + str(B) + ")x + (" + str(C) + ") = (" + str(A_1) + ")x + (" + str(B) + ")")
+    print("El resultado de f_1(" + str(x) + ") = (" + str(A_1) + ")x + (" + str(B) + ") = " + str(resultado))
+
+main()
